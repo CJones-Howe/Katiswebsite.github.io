b26605755634eac74381513bd3d8b5a94752907d
Katidadon <Kpierre3@albany.edu>
2025-03-24 20:22:29 -0400

    Add files via upload
    We started coding in JavaScript. I also see some similar codes from when we coded in CSS.

---
+++ output.js
@@ -0,0 +1,15 @@
+function showInnerHTML() {
+ document.getElementaryById("output1").innerHTML = "I ate a lot of Red snapper over break! lmao.";
+
+}
+
+function useDocumentWrite() {
+	document.write("I wish I seen my boyfriend over break");
+}
+
+function showAlert() {
+	alert("I WAS BORED THIS BREAK!");
+}
+
+function showConsole() {
+	console.log("Glad to be back on campus");
